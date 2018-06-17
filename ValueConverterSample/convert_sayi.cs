43b0327386660f3f4d11a3b68fc22a91b3979871
selcuk <benimpostahesabim@gmail.com>
2018-06-17 14:32:24 +0300

    Proje dosyası ekle.

---
+++ ValueConverterSample/convert_sayi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ValueConverterSample
+{
+    class convert_sayi : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString() == "admin")
+            {
+                return "admin adı girildi, converter çalıştı";
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
